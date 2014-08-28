d8603db87a7fffe41f7520d4bf641b5a0aa76dfc
Marcus Schäfer <ms@suse.de>
2014-08-29 00:13:08 +0200

    Unit tests for job

--- spec/unit/job_spec.rb
+++ spec/unit/job_spec.rb
@@ -1,4 +1,45 @@
 require_relative "spec_helper"
 
 describe Job do
+  before(:each) do
+    expect_any_instance_of(BuildSystem).to receive(:change_working_dir)
+    expect_any_instance_of(BuildSystem).to receive(:get_ip)
+    @job = Job.new(BuildSystem.new("spec/helper/recipe_good"))
+  end
+
+  describe "#initialize" do
+    it "raises if job gets no BuildSystem" do
+      expect { Job.new(File) }.to raise_error
+    end
+  end
+
+  describe "#build" do
+    it "raises if build failed" do
+      expect(@job).to receive(:prepare_build)
+      expect(Command).to receive(:run).and_raise(
+        Cheetah::ExecutionFailed.new(nil, nil, nil, nil)
+      )
+      expect(@job).to receive(:get_buildlog)
+      expect_any_instance_of(BuildSystem).to receive(:halt)
+      expect { @job.build }.to raise_error(Dice::Errors::BuildFailed)
+    end
+  end
+
+  describe "#get_result" do
+    it "raises if result retrieval failed" do
+      expect(Command).to receive(:run).and_raise(
+        Cheetah::ExecutionFailed.new(nil, nil, nil, nil)
+      )
+      expect_any_instance_of(BuildSystem).to receive(:halt)
+      expect { @job.get_result }.to raise_error(
+        Dice::Errors::ResultRetrievalFailed
+      )
+    end
+  end
+
+  describe "#prepare_build" do
+  end
+
+  describe "#get_buildlog" do
+  end
 end
