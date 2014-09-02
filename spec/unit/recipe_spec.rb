af5472112adce07edfaf5fac89463f59fc21b5e0
Marcus Schäfer <ms@suse.de>
2014-09-02 21:39:21 +0200

    Unit tests for missing methods

--- spec/unit/recipe_spec.rb
+++ spec/unit/recipe_spec.rb
@@ -1,13 +1,17 @@
 require_relative "spec_helper"
 
 describe Recipe do
+  before(:each) do
+    @recipe = Recipe.new("spec/helper/recipe_good")
+  end
+
   describe "#initialize" do
     it "raises if description does not exist or is no directory" do
       expect { Recipe.new("foo") }.to raise_error(Dice::Errors::NoDirectory)
     end
 
     it "returns a Recipe for good recipe" do
-      expect(Recipe.new("spec/helper/recipe_good")).to be_a(Recipe)
+      expect(@recipe).to be_a(Recipe)
     end
 
     it "raises if Vagrantfile is missing" do
@@ -23,25 +27,60 @@ describe Recipe do
 
   describe "get_basepath" do
     it "returns absolut path name containing helper/recipe_good" do
-      recipe = Recipe.new("spec/helper/recipe_good")
-      expect(recipe.get_basepath).to match(/^\/.*\/helper\/recipe_good/)
+      expect(@recipe.get_basepath).to match(/^\/.*\/helper\/recipe_good/)
     end
   end
 
   describe "change_working_dir" do
     it "receives a Dir.chdir containing helper/recipe_good" do
-      recipe = Recipe.new("spec/helper/recipe_good")
       expect(Dir).to receive(:chdir).with(/^\/.*\/helper\/recipe_good/)
-      recipe.change_working_dir
+      @recipe.change_working_dir
     end
   end
 
   describe "reset_working_dir" do
     it "receives a Dir.chdir containing current dir" do
       cwd = Dir.pwd
-      recipe = Recipe.new("spec/helper/recipe_good")
       expect(Dir).to receive(:chdir).with(cwd)
-      recipe.reset_working_dir
+      @recipe.reset_working_dir
+    end
+  end
+
+  describe "createDigest" do
+    it "creates expected sha256 digest" do
+      expect(@recipe.createDigest).to eq(
+        "config.xml:e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855\n"
+      )
+    end
+  end
+
+  describe "writeDigest" do
+    it "wants to create .checksum.sha256" do
+      expect(File).to receive(:new).with(".checksum.sha256", "w").
+        and_return(File.new("/tmp/foo", "w"))
+      @recipe.writeDigest("foo")
+    end
+
+    it "want to create .checksum.sha256.xxx" do
+      expect(File).to receive(:new).with(".checksum.sha256.xxx", "w").
+        and_return(File.new("/tmp/foo", "w"))
+      @recipe.writeDigest("foo", ".xxx")
+    end
+  end
+
+  describe "readDigest" do
+    it "reads and returns current digest" do
+      expect(File).to receive(:read).with(".checksum.sha256").
+        and_return("foo")
+      expect(@recipe.readDigest).to eq("foo")
+    end
+  end
+
+  describe "validateDigest" do
+    it "compares two digests" do
+      expect(@recipe).to receive(:readDigest).and_return("foo")
+      expect(@recipe).to receive(:createDigest).and_return("foo")
+      expect(@recipe.validateDigest).to eq(true)
     end
   end
 end
