8360fe33cabf1c75cb408ad50c3373c93783266c
Marcus Schäfer <ms@suse.de>
2014-09-03 22:37:00 +0200

    Unit tests for Solve class

--- lib/solver.rb
+++ lib/solver.rb
@@ -5,19 +5,11 @@ class Solve
     end
     @recipe_path = system.get_basepath
     @recipe_solv = @recipe_path + "/config.scan"
+    @info_pattern = "/tmp/kiwi-xmlinfo*"
   end
 
   def writeScan
-    pattern = "/tmp/kiwi-xmlinfo*"
-    Dir[pattern].each do |file|
-      begin
-        Command.run("sudo", "rm", "-f", file)
-      rescue Cheetah::ExecutionFailed => e
-        raise Dice::Errors::SolveCleanUpFailed.new(
-          "Can't remove tmp data: #{e.stderr}"
-        )
-      end
-    end
+    cleanup
     begin
       Command.run(
         "sudo", "/usr/sbin/kiwi", "--info", @recipe_path,
@@ -28,7 +20,30 @@ class Solve
         "kiwi packager solver failed with:\n #{e.stdout}"
       )
     end
-    result = Dir[pattern].first
+    store_to_receipt
+  end
+
+  private
+
+  def get_info_files
+    return Dir.glob @info_pattern
+  end
+
+  def cleanup
+    file_list = get_info_files
+    file_list.each do |file|
+      begin
+        Command.run("sudo", "rm", "-f", file)
+      rescue Cheetah::ExecutionFailed => e
+        raise Dice::Errors::SolveCleanUpFailed.new(
+          "Can't remove tmp data: #{e.stderr}"
+        )
+      end
+    end
+  end
+
+  def store_to_receipt
+    result = get_info_files.first
     recipe_scan = File.open(@recipe_solv, "w")
     begin
       Command.run("sudo", "cat", result, :stdout => recipe_scan)
--- spec/unit/solver_spec.rb
+++ spec/unit/solver_spec.rb
@@ -1,7 +1,63 @@
 require_relative "spec_helper"
 
 describe Solve do
+  before(:each) do
+    expect_any_instance_of(BuildSystem).to receive(:change_working_dir)
+    system = BuildSystem.new("spec/helper/recipe_good")
+    @solve = Solve.new(system)
+    @recipe_path = system.get_basepath
+  end
+
   describe "#writeScan" do
-#TODO
+    it "raises if kiwi info scan failed" do
+      expect(@solve).to receive(:cleanup)
+      expect(Command).to receive(:run).with(
+        "sudo", "/usr/sbin/kiwi", "--info", @recipe_path,
+        "--select", "packages"
+      ).and_raise(
+        Cheetah::ExecutionFailed.new(nil, nil, nil, nil)
+      )
+      expect { @solve.writeScan }.to raise_error(
+        Dice::Errors::SolvePackagesFailed
+      )
+    end
+  end
+
+  describe "#get_info_files" do
+    it "glob matches a pattern and returns the result" do
+      expect(Dir).to receive(:glob).with("/tmp/kiwi-xmlinfo*").
+        and_return(["foo"])
+      expect(@solve.instance_eval{ get_info_files }).to eq(["foo"])
+    end
+  end
+
+  describe "#cleanup" do
+    it "removes files from get_info_files and raises on error" do
+      expect(@solve).to receive(:get_info_files).and_return(["foo"])
+      expect(Command).to receive(:run).with(
+        "sudo", "rm", "-f", "foo"
+      ).and_raise(
+        Cheetah::ExecutionFailed.new(nil, nil, nil, nil)
+      )
+      expect{ @solve.instance_eval{ cleanup } }.to raise_error(
+        Dice::Errors::SolveCleanUpFailed
+      )
+    end
+  end
+
+  describe "#store_to_receipt" do
+    it "stores first file from info result match and raises on error" do
+      recipe_scan = @recipe_path + "/config.scan"
+      expect(@solve).to receive(:get_info_files).and_return(["foo","xxx"])
+      expect(File).to receive(:open).with(recipe_scan, "w")
+      expect(Command).to receive(:run).with(
+        "sudo", "cat", "foo", :stdout => nil
+      ).and_raise(
+        Cheetah::ExecutionFailed.new(nil, nil, nil, nil)
+      )
+      expect{ @solve.instance_eval{ store_to_receipt } }.to raise_error(
+        Dice::Errors::SolveCreateRecipeResultFailed
+      )
+    end
   end
 end
