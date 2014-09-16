188dfaf652e916d6a05a85468ca6cab00e6584e3
Marcus Schäfer <ms@suse.de>
2014-09-16 15:32:33 +0200

    Update unit tests

--- spec/unit/diceconfig_spec.rb
+++ spec/unit/diceconfig_spec.rb
@@ -1,5 +1,42 @@
 require_relative "spec_helper"
 
 describe DiceConfig do
-  #TODO
+  before(:each) do
+    @config = DiceConfig.new
+  end
+
+  describe "#initialize" do
+    it "sets default values" do
+      buildhost = @config.instance_variable_get(:@buildhost)
+      ssh_private_key = @config.instance_variable_get(:@ssh_private_key)
+      ssh_user = @config.instance_variable_get(:@ssh_user)
+      expect(ssh_user).to eq("vagrant")
+      expect(ssh_private_key).to match(/dice\/key\/vagrant/)
+      expect(buildhost).to eq("__VAGRANT__")
+    end
+  end
+
+  describe "#buildhost=" do
+    it "sets the buildhost instance variable" do
+      @config.buildhost= "bob"
+      buildhost = @config.instance_variable_get(:@buildhost)
+      expect(buildhost).to eq("bob")
+    end
+  end
+
+  describe "#ssh_private_key=" do
+    it "sets the ssh_private_key instance variable" do
+      @config.ssh_private_key= "bob"
+      ssh_private_key = @config.instance_variable_get(:@ssh_private_key)
+      expect(ssh_private_key).to eq("bob")
+    end
+  end
+
+  describe "#ssh_user=" do
+    it "sets the ssh_user instance variable" do
+      @config.ssh_user= "bob"
+      ssh_user = @config.instance_variable_get(:@ssh_user)
+      expect(ssh_user).to eq("bob")
+    end
+  end
 end
--- spec/unit/host_build_system_spec.rb
+++ spec/unit/host_build_system_spec.rb
@@ -1,5 +1,63 @@
 require_relative "spec_helper"
 
 describe HostBuildSystem do
-  #TODO
+  before(:each) do
+    expect_any_instance_of(BuildSystem).to receive(:change_working_dir)
+    @system = HostBuildSystem.new("spec/helper/recipe_good")
+    @host = @system.instance_variable_get(:@host)
+  end
+
+  describe "#up" do
+    it "adds a log information about the used host" do
+      expect(Logger).to receive(:info).with(/#{@host}/)
+      @system.up
+    end
+  end
+
+  describe "#provision" do
+    it "calls rsync to transfer the recipe to the buildhost" do
+      expect(Command).to receive(:run).with(
+        "rsync", "-e", /ssh -i .*key\/vagrant/,
+        "-z", "-a", "-v", "--exclude", ".*", ".",
+        "vagrant@__VAGRANT__:/vagrant", {:stdout=>:capture}
+      ).and_raise(
+        Cheetah::ExecutionFailed.new(nil, nil, nil, nil)
+      )
+      expect(@system).to receive(:halt)
+      expect { @system.provision }.to raise_error(
+        Dice::Errors::HostProvisionFailed
+      )
+    end
+  end
+
+  describe "#halt" do
+    it "resets the working dir" do
+      expect(@system).to receive(:reset_working_dir)
+      @system.halt
+    end
+  end
+
+  describe "#is_locked?" do
+    it "checks lock status via pidof" do
+      expect(Command).to receive(:run).with(
+        "ssh", "-i", /key\/vagrant/, "vagrant@__VAGRANT__",
+        "pidof -x kiwi"
+      ).and_raise(
+        Cheetah::ExecutionFailed.new(nil, nil, nil, nil)
+      )
+      expect(@system.is_locked?).to eq(false)
+    end
+  end
+
+  describe "#get_port" do
+    it "returns standard ssh port" do
+      expect(@system.get_port).to eq("22")
+    end
+  end
+
+  describe "#get_ip" do
+    it "returns ip or host name" do
+      expect(@system.get_ip).to eq(@host)
+    end
+  end
 end
