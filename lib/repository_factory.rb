9cbed5dce0fbb2fad5da0c21386cb9b22294e74b
Marcus Schäfer <ms@suse.de>
2014-11-14 14:29:10 +0100

    Added unit tests for RepositoryFactory

--- lib/repository_factory.rb
+++ lib/repository_factory.rb
@@ -3,7 +3,7 @@ class RepositoryFactory
     def new(uri)
       repo = nil
       begin
-        open(uri + "/repodata/repomd.xml.key", "rb")
+        File.open(uri + "/repodata/repomd.xml.key", "rb")
         repo = RpmMdRepository.new(uri)
       rescue
         repo = SuSERepository.new(uri)
--- spec/unit/repository_factory_spec.rb
+++ spec/unit/repository_factory_spec.rb
@@ -1,5 +1,22 @@
 require_relative "spec_helper"
 
 describe RepositoryFactory do
-  # TODO
+  before(:each) do
+    @rpmmd_repo = double(RpmMdRepository)
+    @suse_repo = double(SuSERepository)
+    allow(RpmMdRepository).to receive(:new).and_return(@rpmmd_repo)
+    allow(SuSERepository).to receive(:new).and_return(@suse_repo)
+  end
+ 
+  describe "#new" do
+    it "creates a RpmMdRepository" do
+      expect(File).to receive(:open).with("/repodata/repomd.xml.key", "rb")
+      expect(RepositoryFactory.new("")).to eq(@rpmmd_repo)
+    end
+
+    it "creates a SuSERepository" do
+      expect(File).to receive(:open).and_raise
+      expect(RepositoryFactory.new("")).to eq(@suse_repo)
+    end
+  end
 end
