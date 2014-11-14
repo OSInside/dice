057c001be745b960e17f16e58b66bb4ed54d78dd
Marcus Schäfer <ms@suse.de>
2014-11-14 11:36:44 +0100

    Added unit tests for KiwiUri

--- spec/unit/kiwi_uri_spec.rb
+++ spec/unit/kiwi_uri_spec.rb
@@ -1,5 +1,23 @@
 require_relative "spec_helper"
 
 describe KiwiUri do
-  # TODO
+  describe "#self.translate" do
+    it "translates dist obs url to download.opensuse.org" do
+      expect(KiwiUri.translate("obs://13.1/repos")).to eq(
+        "http://download.opensuse.org/distribution/13.1/repos/"
+      )
+    end
+
+    it "translates http url by leaving it as it is" do
+      expect(KiwiUri.translate("http://xxx")).to eq(
+        "http://xxx"
+      )
+    end
+
+    it "raises on unknown uri type" do
+      expect { KiwiUri.translate("bob") }.to raise_error(
+        Dice::Errors::UriTypeUnknown
+      )
+    end
+  end
 end
