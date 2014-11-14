c3968ab8e7b8b9bc5733cbf27fa665b775934858
Marcus Schäfer <ms@suse.de>
2014-11-14 11:26:32 +0100

    Added unit tests for KiwiConfig

--- spec/unit/kiwi_config_spec.rb
+++ spec/unit/kiwi_config_spec.rb
@@ -1,5 +1,31 @@
 require_relative "spec_helper"
 
 describe KiwiConfig do
-  # TODO
+  before(:each) do
+    @kiwi_config = KiwiConfig.new("spec/helper")
+  end
+
+  describe "#initialize" do
+    it "reads in a kiwi XML document" do
+      file = double(File)
+      expect(File).to receive(:open).with("foo/config.xml").and_return(file)
+      expect(REXML::Document).to receive(:new).with(file)
+      expect(file).to receive(:close)
+      KiwiConfig.new("foo")
+    end
+  end
+
+  describe "#repos" do
+    it "reads repository/source from XML tree" do
+      result = ["http://bob"]
+      expect(@kiwi_config.repos).to eq(result)
+    end
+  end
+
+  describe "packages" do
+    it "reads packages/package|namedCollection from XML tree" do
+      result = ["a", "b", "c", "pattern:x"]
+      expect(@kiwi_config.packages).to eq(result)
+    end
+  end
 end
