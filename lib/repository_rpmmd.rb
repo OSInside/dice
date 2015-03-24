2c6d873e2641559b5090f189c7f5a1a6cc234513
Marcus Schäfer <ms@suse.de>
2015-03-24 17:05:43 +0100

    Add use of pattern data for rpm-md repos
    In the buildservice an rpm-md repo can provide suse style
    pattern information. If present we make use of them to form
    the solvable

--- lib/repository_rpmmd.rb
+++ lib/repository_rpmmd.rb
@@ -12,16 +12,30 @@ class RpmMdRepository < RepositoryBase
       return solv_file
     end
     tmp_dir = create_tmpdir
+    solv_dir = tmp_dir + "/solv"
     get_repomd_files.each do |repo_file|
       curl_file(
         :source => repo_file,
         :dest   => tmp_dir + "/" + File.basename(repo_file)
       )
     end
-    solv_dir = tmp_dir + "/solv"
     create_solv(
       :tool       => "rpmmd2solv",
       :source_dir => tmp_dir,
+      :dest_dir   => solv_dir
+    )
+    # rpm-md repos created by the buildservice optionaly provides
+    # pattern information in suse style which we add to the solvable
+    # when present
+    get_pattern_files.each do |pattern|
+      curl_file(
+        :source => pattern,
+        :dest   => tmp_dir + "/patterns/" + File.basename(pattern)
+      )
+    end
+    create_solv(
+      :tool       => "susetags2solv",
+      :source_dir => tmp_dir + "/patterns",
       :dest_dir   => solv_dir
     )
     merge_solv(solv_dir, time)
@@ -51,6 +65,25 @@ class RpmMdRepository < RepositoryBase
         href = e.attribute("href").to_s
         result << href
       end
+    end
+    result
+  end
+
+  def get_pattern_files
+    result = []
+    patterns = []
+    patbase = "setup/descr"
+    begin
+      patterns = load_file(patbase + "/patterns").split("\n")
+    rescue
+      # the patterns information is an optional addition
+      # the buildservice could add to an rpm-md repo. if it
+      # does not exist it's not an error
+      return result
+    end
+    patterns.each do |pat|
+      patfile = patbase + "/" + pat
+      result << patfile
     end
     result
   end
--- spec/unit/repository_rpmmd_spec.rb
+++ spec/unit/repository_rpmmd_spec.rb
@@ -35,6 +35,17 @@ describe RpmMdRepository do
       expect(@repo).to receive(:create_solv).with(
         :tool => "rpmmd2solv", :source_dir => "tmp", :dest_dir => "tmp/solv"
       )
+      expect(@repo).to receive(:get_pattern_files).and_return(["a", "b"])
+      expect(@repo).to receive(:curl_file).with(
+        :source => "a", :dest => "tmp/patterns/a"
+      )
+      expect(@repo).to receive(:curl_file).with(
+        :source => "b", :dest => "tmp/patterns/b"
+      )
+      expect(@repo).to receive(:create_solv).with(
+        :tool => "susetags2solv", :source_dir => "tmp/patterns",
+        :dest_dir => "tmp/solv"
+      )
       expect(@repo).to receive(:merge_solv).with("tmp/solv", "today")
       expect(@repo).to receive(:cleanup)
       expect(@repo.solvable).to match(@meta.solv)
